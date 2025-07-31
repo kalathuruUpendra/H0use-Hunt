cbec245a501d43e59464d4c806283b4cf78485f8
Kalathuru Upendra <145739389+kalathuruUpendra@users.noreply.github.com>
2025-08-01 02:13:21 +0530

    Update Register.jsx

--- frontend/src/modules/common/Register.jsx
+++ frontend/src/modules/common/Register.jsx
@@ -32,7 +32,7 @@ const Register = () => {
     e.preventDefault()
     if (!data?.name || !data?.email || !data?.password||! data?.type ) return alert("Please fill all fields");
     else {
-      axios.post('http://localhost:8001/api/user/register', data)
+      axios.post('https://h0use-hunt.onrender.com/api/user/register', data)
         .then((response) => {
           if (response.data.success) {
             message.success(response.data.message);
