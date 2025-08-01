255fd426a3e3e3b03d6851ab29b202384a984f79
Kalathuru Upendra <145739389+kalathuruUpendra@users.noreply.github.com>
2025-08-01 09:07:42 +0530

    Update RenterHome.jsx

--- frontend/src/modules/user/renter/RenterHome.jsx
+++ frontend/src/modules/user/renter/RenterHome.jsx
@@ -6,7 +6,7 @@ import { UserContext } from '../../../App';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
+//import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import AllPropertiesCards from '../AllPropertiesCards';
 import AllProperty from './AllProperties';
